de9e6c0aea4971bf9d8c181f251f0d8f3e22478d
cnquistador <cnquistador@gmail.com>
2018-06-15 17:06:47 -0600

    4-126 Bug

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Tank.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
 
@@ -14,7 +15,8 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-	
+public:
+	ATank * GetControlledTank() const;
 	
 	
 };
