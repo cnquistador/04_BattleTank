72552bb20c90165e1f5520d0d3d1d62c8b632913
cnquistador <cnquistador@gmail.com>
2018-10-09 13:58:16 -0600

    4-159: EditAnywhere vs EditDefaultsOnly

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -6,53 +6,25 @@
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
-
-	auto ControlledTank = GetControlledTank();
-
-	if (!ControlledTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller not possessing a tank."));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller possessing: %s"), *(ControlledTank->GetName()));
-	}
-
-	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
-
-	if (!PlayerTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller not detecting the player tank."));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller is detecting: %s"), *(PlayerTank->GetName()));
-	}
 }
 
 //Called every frame
 void ATankAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	if (GetPlayerTank())
+
+	auto PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	auto ControlledTank = Cast<ATank>(GetPawn());
+
+	if (PlayerTank)
 	{
 		//TODO Move towards the player
-		GetControlledTank()->AimAt(GetPlayerTank()->GetActorLocation());
-		//Fire if ready
+
+		//Aim towards the player
+		ControlledTank->AimAt(PlayerTank->GetActorLocation());
+		
+		ControlledTank->Fire(); //TODO Limit firing rate
 	}
 
 	return;
-}
-
-ATank* ATankAIController::GetControlledTank() const
-{
-	return Cast<ATank>(GetPawn());
-}
-
-ATank* ATankAIController::GetPlayerTank() const
-{
-	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
-	if (!PlayerPawn) { return nullptr;  }
-	return Cast<ATank>(GetPawn());
 }
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -17,13 +17,8 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
-
-private:
-	ATank* GetControlledTank() const;
-	
-	ATank* GetPlayerTank() const;
 };
